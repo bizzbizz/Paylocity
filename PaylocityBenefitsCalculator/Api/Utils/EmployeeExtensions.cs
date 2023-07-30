6c4844b437cc37c9b916fa9052e0a02b1ad9ca85
Bizhan Aminipour <b.aminipour@quadient.com>
2023-07-30 12:54:59 +0200

    🪲 Added Relationship.None to illegitimate dependents

--- PaylocityBenefitsCalculator/Api/Utils/EmployeeExtensions.cs
+++ PaylocityBenefitsCalculator/Api/Utils/EmployeeExtensions.cs
@@ -6,7 +6,8 @@ public static class EmployeeExtensions
 {
     //This method is the only place we write this logic
     public static bool HasIllegitimateDependents(this Employee employee) =>
-        employee.Dependents.Count(d => d.Relationship is Relationship.Spouse or Relationship.DomesticPartner) > 1;
+        employee.Dependents.Count(d => d.Relationship is Relationship.Spouse or Relationship.DomesticPartner) > 1
+        || employee.Dependents.Any(d => d.Relationship is Relationship.None);
 
     //A fancy way of reading Dependents and ensuring valid data. Normally we expect to get a nice validation error.
     // But this is useful because we make sure we never produce incorrect data.
